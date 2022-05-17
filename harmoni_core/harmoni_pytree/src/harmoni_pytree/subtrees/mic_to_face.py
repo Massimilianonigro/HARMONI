5084a45d65ffc7ee37a8fde71b009acf4cbde531
Harsh Shah <prodigy.harsh18@gmail.com>
2022-05-17 12:26:45 +0530

    added subtrees and launch files in harmoni_ptree

--- harmoni_core/harmoni_pytree/src/harmoni_pytree/subtrees/mic_to_face.py
+++ harmoni_core/harmoni_pytree/src/harmoni_pytree/subtrees/mic_to_face.py
@@ -11,13 +11,20 @@ import py_trees
 import time
 import rospy
 from random import randint
+import subprocess
+import operator
 import py_trees.console as console
+import running_or_failure as rf
 
 from harmoni_common_lib.constants import *
 
-from harmoni_pytree.leaves.speaker_service import SpeakerServicePytree
+from harmoni_pytree.leaves.deep_stt import DeepSpeechToTextServicePytree
+from harmoni_pytree.leaves.aws_lex_analyzer_service import AWSLexAnalyzerServicePytree
+from harmoni_pytree.leaves.aws_lex_trigger_service import AWSLexTriggerServicePytree
 from harmoni_pytree.leaves.aws_tts_service import AWSTtsServicePytree
 from harmoni_pytree.leaves.facial_exp_service import FacialExpServicePytree
+from harmoni_pytree.leaves.microphone_service import MicrophoneServicePytree
+from harmoni_pytree.leaves.speaker_service import SpeakerServicePytree
 
 ##############################################################################
 # Classes
@@ -27,7 +34,7 @@ from harmoni_pytree.leaves.facial_exp_service import FacialExpServicePytree
 def description(root):
     content = "\n\n"
     content += "\n"
-    content += "Tree having sequential children: Text-to-Speech and Speaker\n"
+    content += "EVENTS\n"
     if py_trees.console.has_colours:
         banner_line = console.green + "*" * 79 + "\n" + console.reset
         s = "\n"
@@ -77,15 +84,21 @@ def post_tick_handler(snapshot_visitor, behaviour_tree):
     print(py_trees.display.unicode_blackboard())
 
 
-def create_root(name= "Face_Polly"):
+def create_root(name= "MicAndSTT"):
 
-    tts = AWSTtsServicePytree("TTSactivity")
+    microphone = MicrophoneServicePytree("MicrophoneMainActivity")
+    deep_stt = DeepSpeechToTextServicePytree("SpeechToTextMainActivity")
     speaker = SpeakerServicePytree("SpeakerActivity")
+    tts = AWSTtsServicePytree("TTSActivity")
+    chatbot_analyzer = AWSLexAnalyzerServicePytree("AwsLexAnalyzerPyTreeActivity")
+    chatbot_trigger = AWSLexTriggerServicePytree("AwsLexTriggerPyTreeActivity")
     face = FacialExpServicePytree("FaceActivity")
     face_speaker = py_trees.composites.Parallel(name="face_speaker")
     face_speaker.add_children([speaker, face])
-    root = py_trees.composites.Sequence(name=name)
-    root.add_children([tts, face_speaker])
+
+
+    root = py_trees.composites.Sequence(name="mini_bot")
+    root.add_children([microphone, deep_stt, chatbot_analyzer, chatbot_trigger, tts, face_speaker])
 
     return root
 
@@ -94,23 +107,22 @@ def create_root(name= "Face_Polly"):
 ##############################################################################
 
 def main():
+    """
+    Entry point for the demo script.
+    """
     py_trees.logging.level = py_trees.logging.Level.DEBUG
     root = create_root()
     print(description(root))
 
-    blackboard_tts = py_trees.blackboard.Client(name="blackboard_tts", namespace=ActuatorNameSpace.tts.name)
-    blackboard_tts.register_key("result", access=py_trees.common.Access.READ)
-    print(blackboard_tts)
+    blackboardProva = py_trees.blackboard.Client(name="blackboardProva", namespace=DetectorNameSpace.stt.name)
+    blackboardProva.register_key("result", access=py_trees.common.Access.READ)
+    print(blackboardProva)
+        
+    ####################
+    # Tree Stewardship
+    ####################
 
-    blackboard_bot = py_trees.blackboard.Client(name="blackboard_bot", namespace=DialogueNameSpace.bot.name +"/"+PyTreeNameSpace.trigger.name)
-    blackboard_bot.register_key("result", access=py_trees.common.Access.READ)
-    print(blackboard_bot)
-
-    blackboard_scene = py_trees.blackboard.Client(name="blackboard_scene", namespace=PyTreeNameSpace.scene.name)
-    blackboard_scene.register_key("face_exp", access=py_trees.common.Access.READ)
-    print(blackboard_scene)
-
-    rospy.init_node("face_polly_node", log_level=rospy.INFO)
+    rospy.init_node("test_default", log_level=rospy.INFO)
 
     behaviour_tree = py_trees.trees.BehaviourTree(root)
     behaviour_tree.add_pre_tick_handler(pre_tick_handler)
@@ -127,10 +139,7 @@ def main():
     for unused_i in range(1, 30):
         try:
             behaviour_tree.tick()
-            time.sleep(1)
-            print(blackboard_tts)
-            print(blackboard_bot)
-            print(blackboard_scene)
+            time.sleep(3)
         except KeyboardInterrupt:
             break
     print("\n")
